a77f7850c58f062d2f4919b52b41bcf6fb5d637e
athiravsnair <mailathiravsnair@gmail.com>
2021-03-05 16:20:28 +0530

    Second Commit

--- DemoGitApp/src/com/faith/app/DemoApp.java
+++ DemoGitApp/src/com/faith/app/DemoApp.java
@@ -5,6 +5,7 @@ public class DemoApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Git hub");
+		System.out.println("Git push successful");
 	}
 
 }
