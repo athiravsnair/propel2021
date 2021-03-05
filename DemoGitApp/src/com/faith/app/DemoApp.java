311df2fa219c20d7cf85e4f94c8bba90ad697660
athiravsnair <mailathiravsnair@gmail.com>
2021-03-05 16:07:05 +0530

    First Commit

---
+++ DemoGitApp/src/com/faith/app/DemoApp.java
@@ -0,0 +1,10 @@
+package com.faith.app;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git hub");
+	}
+
+}
